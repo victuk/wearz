7ad5696f21f698d8427afade13f80035280b7fe9
victuk <ukokjnr@gmail.com>
2022-02-23 16:06:38 +0100

    Push work to production

--- wardrobe/assets/base-url.js
+++ wardrobe/assets/base-url.js
@@ -1,11 +1,13 @@
 // let baseUrl = 'http://localhost:4000/v1/';
-
-let cloudinaryUrl = 'https://api.cloudinary.com/v1_1/dae4sosbl/image/upload';
-
 // let cloudinaryPreset = 'p2j2cals';
-
-let baseUrl = 'http://localhost:5000/v1/';
-
 // let cloudinaryUrl = 'https://api.cloudinary.com/v1_1/realitypacefoundation/image/upload';
 
+
+let baseUrl = 'https://wearz.herokuapp.com/v1/';
+let cloudinaryUrl = 'https://api.cloudinary.com/v1_1/dae4sosbl/image/upload';
 let cloudinaryPreset = 'wearzPreset';
+
+
+// let baseUrl = 'http://localhost:5000/v1/';
+// let cloudinaryUrl = 'https://api.cloudinary.com/v1_1/dae4sosbl/image/upload';
+// let cloudinaryPreset = 'wearzPresetDev';
