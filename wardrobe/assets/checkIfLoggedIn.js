a1170c6be8c62b8800fc8816de2c5ad1bd0a416c
victuk <ukokjnr@gmail.com>
2022-02-23 16:04:46 +0100

    Push work to production

--- wardrobe/assets/checkIfLoggedIn.js
+++ wardrobe/assets/checkIfLoggedIn.js
@@ -1,27 +1,32 @@
+let logoutButton = document.getElementById('logoutButton');
+
 async function checkLogin() {
 
-    if (localStorage.getItem('token') == null) {
+    if (localStorage.getItem('wearzToken') == null) {
         location.replace('/login.html');
     } else {
-        let url = 'https://realitypacesettersfoundation.herokuapp.com/v1/users/profile';
+        let url = 'http://localhost:5000/v1/users/profile';
 
         let req = await fetch(url, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
-                token: localStorage.getItem('token')
+                token: localStorage.getItem('wearzToken')
             }
         });
 
         req.json().then(r => {
             
             if (r.success == false) {
-                location.replace('/admin/login.html');
-            } else {
-                console.log('Request successful.');
+                location.replace('/login.html');
             }
         });
     }
 }
 
 checkLogin();
+
+logoutButton.onclick = function () {
+    localStorage.removeItem('wearzToken');
+    location.replace('/index.html');
+}
