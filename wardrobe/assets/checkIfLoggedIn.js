4920daf30769f7fd89bb30aef01b55ffe0a81c3d
victuk <ukokjnr@gmail.com>
2022-02-25 12:11:08 +0100

    Password reset and forgot password pages and functionalities added.

--- wardrobe/assets/checkIfLoggedIn.js
+++ wardrobe/assets/checkIfLoggedIn.js
@@ -5,7 +5,7 @@ async function checkLogin() {
     if (localStorage.getItem('wearzToken') == null) {
         location.replace('/login.html');
     } else {
-        let url = 'http://localhost:5000/v1/users/profile';
+        let url = 'https://wearz.herokuapp.com/v1/users/profile';
 
         let req = await fetch(url, {
             method: 'GET',
