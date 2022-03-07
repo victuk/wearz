44a5f5b59810bdd14f7a79de3f8d59faa41bb12f
Victor <ukokjnr@gmail.com>
2022-03-07 04:59:46 +0100

    Working with ML

--- wardrobe/assets/base-url.js
+++ wardrobe/assets/base-url.js
@@ -3,11 +3,11 @@
 // let cloudinaryUrl = 'https://api.cloudinary.com/v1_1/realitypacefoundation/image/upload';
 
 
-let baseUrl = 'https://wearz.herokuapp.com/v1/';
-let cloudinaryUrl = 'https://api.cloudinary.com/v1_1/dae4sosbl/image/upload';
-let cloudinaryPreset = 'wearzPreset';
-
-
-// let baseUrl = 'http://localhost:5000/v1/';
+// let baseUrl = 'https://wearz.herokuapp.com/v1/';
 // let cloudinaryUrl = 'https://api.cloudinary.com/v1_1/dae4sosbl/image/upload';
-// let cloudinaryPreset = 'wearzPresetDev';
+// let cloudinaryPreset = 'wearzPreset';
+
+
+let baseUrl = 'http://localhost:5000/v1/';
+let cloudinaryUrl = 'https://api.cloudinary.com/v1_1/dae4sosbl/image/upload';
+let cloudinaryPreset = 'wearzPresetDev';
