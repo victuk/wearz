3872269b33e3d90130ea7753b891cceba04e03ba
Victor <ukokjnr@gmail.com>
2022-03-26 20:34:39 +0100

    Add recommend

--- wardrobe/assets/checkIfLoggedIn.js
+++ wardrobe/assets/checkIfLoggedIn.js
@@ -5,8 +5,8 @@ async function checkLogin() {
     if (localStorage.getItem('wearzToken') == null) {
         location.replace('/login.html');
     } else {
-        let url = 'https://wearz.herokuapp.com/v1/users/profile';
-        // let url = 'http://localhost:5000/v1/users/profile';
+        // let url = 'https://wearz.herokuapp.com/v1/users/profile';
+        let url = 'http://localhost:5000/v1/users/profile';
         let req = await fetch(url, {
             method: 'GET',
             headers: {
